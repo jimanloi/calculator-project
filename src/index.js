2de595bdcf3e657d0f011b98a97e7007d51db34f
jimanloi <jimanloi@hotmail.com>
2025-02-21 16:35:18 +0100

    created calculator structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('Hello, world!');
+
+const display = document.getElementById('display');
+
+const buttons = document.getElementById('buttons');
+
+const data = [
+    ['C', '%', '+/-', '÷'],
+    ['7', '8', '9', '*'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '=']
+];
+
+data.forEach((row) => {
+    const rowElement = document.createElement('div');
+    rowElement.classList.add('row');
+
+    row.forEach((buttonData) => {
+        const btn = document.createElement('button');
+        btn.innerText = buttonData;
+        rowElement.append(btn);
+    });
+
+    buttons.append(rowElement);
+});
